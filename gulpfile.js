9dc7f98c130f405e04ea4c7b68d6fac747a8c11a
German Hernandez <muygrafico@gmail.com>
2016-01-25 14:26:30 -0500

    add template logic with nunjucksJS | add livereload (run gulp watch)''

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,37 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    livereload = require('gulp-livereload');
+sass = require('gulp-sass'),
+nunjucksRender = require('gulp-nunjucks-render'),
+livereload = require('gulp-livereload');
 
 // gulp.task('default', function() { });
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(livereload());
+  gulp.src('app/sass/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('app/css'))
+  .pipe(livereload());
 });
 
-gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(livereload());
-});
+gulp.task('nunjucks', function() {
+  nunjucksRender.nunjucks.configure(['app/templates/']);
 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./sass/**/*.scss', ['sass']);
-// });
+  // Gets .html and .nunjucks files in pages
+  return gulp.src('app/pages/**/*.+(html|nunjucks)')
+  // Renders template with nunjucks
+  .pipe(nunjucksRender())
+  // output files in app folder
+  .pipe(gulp.dest('app'))
+  .pipe(livereload());
+});
 
 gulp.task('watch', function() {
+
   livereload({ start: true });
   livereload.listen();
   // gulp.watch('less/*.less', ['less']);
-  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/pages/**/*.+(html|nunjucks)', ['nunjucks']);
 
 });
