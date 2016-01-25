bb8739e971e8ae849b098f16d7beb7389db6dc21
German Hernandez <muygrafico@gmail.com>
2016-01-25 12:23:38 -0500

    add livereload | styling nav

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,33 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
 
 // gulp.task('default', function() { });
 
 gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(livereload());
+});
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
+
+gulp.task('watch', function() {
+  livereload({ start: true });
+  livereload.listen();
+  // gulp.watch('less/*.less', ['less']);
   gulp.watch('./sass/**/*.scss', ['sass']);
+
 });
